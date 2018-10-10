c16062468dbaed7ac5f3b1b8b9626d02eb0f572c
Nathan Nguyen <nathanrichardnguyen@gmail.com>
2018-10-10 16:11:40 +1100

    imageview incorrect
    Imageview was miscategorised

--- app/src/main/java/com/example/nathanrnguyen/flashcode/MainActivity.java
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.nathanrnguyen.flashcode;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         lpbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+                startActivity(new Intent(getApplicationContext(), MenuActivity.class));
+                Log.d(TAG, "onClick: start button clicked");
             }
         });
     }
--- app/src/main/java/com/example/nathanrnguyen/flashcode/MenuActivity.java
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/MenuActivity.java
@@ -6,16 +6,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button homeButton = (Button) findViewById(R.id.homeButton);
-    ListView menulv = (ListView) findViewById(R.id.menulv);
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        ImageView homeButton = findViewById(R.id.homeButton);
+        ListView menulv =  findViewById(R.id.menulv);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
