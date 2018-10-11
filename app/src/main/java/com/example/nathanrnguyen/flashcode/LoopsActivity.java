7f74c5bd1618f671c86bff48933c9361c82ef552
Rebecca Vu <rebeccavu98@gmail.com>
2018-10-11 15:52:30 +1100

    loopactivity made with theory/test cards intents built to next activity blank theory and test activities made

--- app/src/main/java/com/example/nathanrnguyen/flashcode/LoopsActivity.java
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/LoopsActivity.java
@@ -1,12 +1,53 @@
 package com.example.nathanrnguyen.flashcode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class LoopsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loops_activity);
+
+        ImageView backButton = findViewById(R.id.backButton);
+        ImageView homeButton = findViewById(R.id.homeButton);
+
+        CardView learnCard = findViewById(R.id.learnCard);
+        CardView testCard = findViewById(R.id.testCard);
+
+        learnCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoopsActivity.this, TheoryActivity.class));
+            }
+        });
+
+        testCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoopsActivity.this, TestActivity.class));
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoopsActivity.this, MenuActivity.class));
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoopsActivity.this, MenuActivity.class));
+            }
+        });
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/TestActivity.java
@@ -0,0 +1,4 @@
+package com.example.nathanrnguyen.flashcode;
+
+public class TestActivity {
+}
