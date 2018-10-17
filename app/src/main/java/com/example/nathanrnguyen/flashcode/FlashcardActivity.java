1ccfce24ec39db1092413b65f939242649f8ca11
Nathan Nguyen <nathanrichardnguyen@gmail.com>
2018-10-17 13:03:52 +1100

    Flashcard code added

--- app/src/main/java/com/example/nathanrnguyen/flashcode/FlashcardActivity.java
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/FlashcardActivity.java
@@ -1,14 +1,28 @@
 package com.example.nathanrnguyen.flashcode;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.vision.barcode.Barcode;
+
 
 public class FlashcardActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
+    private static final String TAG = "FlashcardActivity";
+
 
     ImageView questionicon;
     ImageView answericon;
@@ -16,6 +30,7 @@ public class FlashcardActivity extends AppCompatActivity {
     TextView answertext;
     CardView answercard;
     CardView questioncard;
+    Button scanbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +38,33 @@ public class FlashcardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flashcard);
         questiontext = findViewById(R.id.questiontext);
         answertext = findViewById(R.id.answertext);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: Camera permissions not set");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+            }
+
+        scanbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ScanActivity.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
 
 
+    }
+    //If the camera activity returns a result, this method is run
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            final Barcode qrcode = data.getParcelableExtra("barcoderesult");
+            answertext.post(new Runnable() {
+                @Override
+                public void run() {
+                    answertext.setText(qrcode.displayValue);
+                }
+            });
+        }
 
     }
 }
