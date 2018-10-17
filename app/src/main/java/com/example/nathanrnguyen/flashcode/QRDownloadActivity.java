2329c2f26e7295c58bcbfdb35dcee086c1ce77a7
Rebecca Vu <rebeccavu98@gmail.com>
2018-10-17 12:41:14 +1100

    qrdownloadactivity testactivity updated

---
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/QRDownloadActivity.java
@@ -0,0 +1,20 @@
+package com.example.nathanrnguyen.flashcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class QRDownloadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrdownload);
+
+        ImageView backButton = findViewById(R.id.backButton);
+        ImageView homeButton = findViewById(R.id.homeButton);
+        ImageView
+    }
+
+
+}
--- app/src/main/java/com/example/nathanrnguyen/flashcode/TestActivity.java
+++ app/src/main/java/com/example/nathanrnguyen/flashcode/TestActivity.java
@@ -1,13 +1,44 @@
 package com.example.nathanrnguyen.flashcode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.ImageView;
 
 public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_activity);
+
+        ImageView backButton = findViewById(R.id.backButton);
+        ImageView homeButton = findViewById(R.id.homeButton);
+
+        CardView quizCard = findViewById(R.id.quizCard);
+        CardView flashCard = findViewById(R.id.flashCard);
+
+        flashCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TestActivity.this, QRDownloadActivity.class));
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TestActivity.this, MenuActivity.class));
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(TestActivity.this, MenuActivity.class));
+            }
+        });
 
     }
 }
